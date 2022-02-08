9ff66fde449f2a842a6b000224b476fd826fec4b
Unc-Raymus <31295043+Unc-Raymus@users.noreply.github.com>
2022-02-07 19:01:23 -0500

    Created Quote App

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # random-quotes
+
+simple nodejs application for a class that prints random quotes.
+
+## favorite quotation
+
+"Cling to nothing, not even the teachings."
---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = ["\"Cling to nothing, not even the teachings.\"",
+"\"Lord, I am not worthy to receive you. But only say the word, and I shall be healed.\"",
+"\"Live, Laugh, Love\""]
+
+// console.log(Math.floor((3 *Math.random())))
+let index = Math.floor((3 *Math.random()))
+console.log(quotes[index])
